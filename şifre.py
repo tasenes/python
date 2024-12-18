339701d5698491b4bf12dffd0036324e8db7ea33
tasenes <jhkhjljl808@gmail.com>
2024-12-18 14:09:32 +0200

    Update şifre.py

--- şifre.py
+++ şifre.py
@@ -1,8 +1,10 @@
 def sifre_dogrulama():
+    deneme_sayisi = 0
     while True:
         sifre = input("Şifreyi girin: ")
-        if sifre == "Python":
-            print("Giriş başarılı")
+        deneme_sayisi += 1
+        if sifre == "Enes":
+            print(f"Giriş başarılı. Şifreyi {deneme_sayisi} denemede girdiniz.")
             break
         else:
             print("Tekrar deneyiniz")
