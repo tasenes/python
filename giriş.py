cdaf804ae3fa26d63ab26f6d79a506583207083f
tasenes <jhkhjljl808@gmail.com>
2024-12-25 00:43:13 +0300

    Create giriş.py

---
+++ giriş.py
@@ -0,0 +1,12 @@
+kullanıcı_adı=input("kullanıcı adı:")
+
+if (kullanıcı_adı != "bukre"):
+    print (" kullanıcı adı yanlış")
+
+
+sifre=input("sifre:")
+
+if (sifre != "116"):
+    print ("sifre yanlıs")
+if (kullanıcı_adı == "bukre" and sifre == "116" ):
+    print ("giriş başarılı")
