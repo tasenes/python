56e9f36138bafde7fac7956fefa6d7b1861ba829
tasenes <jhkhjljl808@gmail.com>
2024-12-25 00:49:54 +0300

    Create not ortalama.py

---
+++ not ortalama.py
@@ -0,0 +1,31 @@
+'''
+not1 = float(input("1. notu girin: "))
+
+not2 = float(input("2. notu girin: "))
+
+not3 = float(input("3. notu girin: "))
+
+ortalama = (not1 + not2 + not3) / 3
+
+print("ortalama")
+'''
+#dikdörtgenin çevresi
+'''
+uzun_kenar = float(input("uzun kenarı giriniz:"))
+kısa_kenar = float(input("kısa kenarı giriniz:"))
+cevre = (uzun_kenar*2 + kısa_kenar*2)
+print("dikgörtgenin çevresi:" , cevre)
+alan = (uzun_kenar*kısa_kenar)
+print("dikdörtgenin alanı:" , alan)
+'''
+#karesi
+'''
+sayı = float(input("sayı giriniz:"))
+sayının_karesi = sayı**2
+print(f"{sayı} 'nın karesi {sayının_karesi}'dir")
+'''
+#yaş
+dogum_tarihi = int(input("dogum tarihini gir:"))
+adınız =input("adınızı girin:")
+yas = (2024-dogum_tarihi)
+print (yas)
