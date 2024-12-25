d186bf9ed43ce59cbb1acd8b94c3bcef99f340f5
tasenes <jhkhjljl808@gmail.com>
2024-12-25 12:48:55 +0200

    Create araba markası sordurma.py

---
+++ araba markası sordurma.py
@@ -0,0 +1,22 @@
+araba= {
+   0:{
+       "üretici":"Fiat",
+       "marka":"doblo",
+       "model":"2020",
+       "motor gücü":"2.0",
+       "fiyat":"500.000",
+       "KM":"300.000",
+       "yakıtı":"lgp"
+       }
+  ,1:{
+       "üretici":"fiat",
+       "marka":"tofaş",
+       "model":"2012",
+       "motor gücü": "1.6",
+       "fiyat":"150.000",
+       "KM":"500.000",
+       "yakıtı":"mazot"
+       }
+}
+
+print(araba[1]['motor gücü'])
