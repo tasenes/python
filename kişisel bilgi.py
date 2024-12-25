6dcda4db979b4eb339a3955337ed17f4d1e01610
tasenes <jhkhjljl808@gmail.com>
2024-12-25 12:52:22 +0200

    Create kişisel bilgi.py

---
+++ kişisel bilgi.py
@@ -0,0 +1,6 @@
+kisisel_bilgileri = ["osman","karaosman","14","168"]
+egitim_bilgileri = ["Necla Görer","Ş.A.T.O","B.A.K"]
+adres_bilgileri = ["Güzelyurt 2102 sok.","Küçük Çekmece kanarya","beylikdüzü"]
+
+print(kisisel_bilgileri,egitim_bilgileri,adres_bilgileri)
+#['osman', 'karaosman', '14', '168'] ['Necla Görer', 'Ş.A.T.O', 'B.A.K'] ['Güzelyurt 2102 sok.', 'Küçük Çekmece kanarya', 'beylikdüzü']
