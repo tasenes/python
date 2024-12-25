bc8196d9ee605f6820a9495bb31bb22629ed3faa
tasenes <jhkhjljl808@gmail.com>
2024-12-25 12:51:54 +0200

    Create karışık.py

---
+++ karışık.py
@@ -0,0 +1,32 @@
+'''not1 = int(input("1. sınav notunu giriniz (sayı): "))
+not2 = int(input("2. sınav notunu giriniz (sayı): "))
+not3 = int(input("3. sınav notunu giriniz (sayı): "))
+
+ortalama = (not1 + not2 + not3) / 3
+
+print("notların ortalaması: " + str(ortalama))
+----------------------------------------------'''
+kisakenar=int(input("kisa kenarı giriniz: " ))
+uzunkenar=int(input("uzun kenarı giriniz: " ))
+çevre=kisakenar*2 + uzunkenar*2
+alan=kisakenar*uzunkenar
+print("dikdörtgenin çevresi: ")
+print(çevre)
+print("dikdörtgenin alanı: " )
+print(alan)
+'''----------------------------------------------
+sayi=int(input("sayi giriniz: "))
+
+sayi**=2
+
+print(sayi)
+----------------------------------------------
+ad = input("adınızı giriniz: ")
+dogum_tarıhı = int(input("doğum tarihinizi giriniz:"))
+yil = 2024
+yas = yil - dogum_tarıhı
+
+print("merhaba " + ad + " yaşınız: " + str(yas))
+-------------------------------------------------
+ad = input("adınızı giriniz: ")
+input ("merhaba " + ad + " bu yıl kaçitap okudun?: ")'''
