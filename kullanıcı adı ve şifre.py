b1ebdd7250a8ec3ba80522643929e53c248d175e
tasenes <jhkhjljl808@gmail.com>
2024-12-25 12:51:24 +0200

    Create kullanıcı adı ve şifre.py

---
+++ kullanıcı adı ve şifre.py
@@ -0,0 +1,10 @@
+kullanici_adi=input("kullanıcı adı: ")
+sifre=input("şifre: ")
+if(kullanici_adi!="osman"):
+    print("isim yanlış")
+if(sifre!="1234"):
+    print("şifre yanlış")
+if(kullanici_adi == "osman"):
+    print("isim doğru")
+if(sifre == "1234"):
+    print("şifre doğru")
