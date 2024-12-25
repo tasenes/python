d1f44098aedf8402953191a37be369c73ac83d23
tasenes <jhkhjljl808@gmail.com>
2024-12-25 12:46:35 +0200

    Create haftanın günleri.py

---
+++ haftanın günleri.py
@@ -0,0 +1,8 @@
+hafatanin_gunleri=['haftanın 1.günü:pzt','haftanın 2.günü:salı','haftanın 3.günü:çar','haftanın 4.günü:per','haftanın 5.günü:cum','haftanın 6.günü:cmt','haftanın 7.günü:pzr']
+print(hafatanin_gunleri[0])
+print(hafatanin_gunleri[1])
+print(hafatanin_gunleri[2])
+print(hafatanin_gunleri[3])
+print(hafatanin_gunleri[4])
+print(hafatanin_gunleri[5])
+print(hafatanin_gunleri[6])
