4535f72f9432bb713739fbd540b20dc5798cfef5
tasenes <jhkhjljl808@gmail.com>
2024-12-25 12:50:30 +0200

    Create aziz sancar.py

---
+++ aziz sancar.py
@@ -0,0 +1,7 @@
+aziz_sancar={"basarısı": "nobel ödülünü aldı","dogum_yeri": "Mardin","çalışması": "DNA onarımının nasıl yapılıcağını buldu","ailesi": "mardinde bir çiftçi","ilgi duydugu spor": "Fubol"}
+
+print(aziz_sancar['basarısı'])
+print(aziz_sancar['dogum_yeri'])
+print(aziz_sancar['çalışması'])
+print(aziz_sancar['ailesi'])
+print(aziz_sancar['ilgi duydugu spor'])
